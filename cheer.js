385040d21b6180a33685aa879847f52b278f252e
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-22 21:52:13 -0500

    handled multiple names

--- cheer.js
+++ cheer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var includes = require('lodash/collection/includes')
 var prompt = require('prompt');
 
 prompt.get('name', function (err, result) {
@@ -7,11 +8,23 @@ prompt.get('name', function (err, result) {
 });
 
 function printCheer(name) {
-     console.log('Hello, ' + name + '!');
-    var options = {
-      encoding: 'utf8'
-    };
-    var letters = name.toUpperCase().split('');
-    for (var i = 0; i < letters.length; i++)
-      process.stdout.write('Give me a ' + letters[i] + '!\n'); //more direct way to write to the console;
-    }
+  console.log('All right, it\'s ' + name + '!');
+
+  var letters = name.toUpperCase().split('');
+  //get rid of any whitespace
+  letters = letters.filter(function (str) {
+    return /\S/.test(str);
+  });
+  for (var i = 0; i < letters.length; i++)
+
+
+  /*var anLetters = ['AEFHILMNORSX'];
+     if(var article = letters[i] == _.includes(anLetters, letters[i]){
+           return 'an';
+           } else {
+           return 'a';
+           }
+          );*/
+
+    process.stdout.write('Give me a ' + letters[i] + '!\n'); //more direct way to write to the console;
+}
