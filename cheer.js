b72a755920b8a44b6d772bf0e300dd6ebfd80d8c
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-27 14:54:34 -0500

    Working cheer

--- cheer.js
+++ cheer.js
@@ -1,30 +1,23 @@
 #!/usr/bin/env node
-
-var includes = require('lodash/collection/includes')
+ //var includes = require('lodash/collection/includes')
 var prompt = require('prompt');
-
 prompt.get('name', function (err, result) {
   printCheer(result.name);
 });
 
 function printCheer(name) {
   console.log('All right, it\'s ' + name + '!');
-
   var letters = name.toUpperCase().split('');
-  //get rid of any whitespace
+  //filter removes the white spaces
+  //returned by indexOf
   letters = letters.filter(function (str) {
-    return /\S/.test(str);
+    return str.indexOf(' ');
   });
-  for (var i = 0; i < letters.length; i++)
-
-
-  /*var anLetters = ['AEFHILMNORSX'];
-     if(var article = letters[i] == _.includes(anLetters, letters[i]){
-           return 'an';
-           } else {
-           return 'a';
-           }
-          );*/
-
-    process.stdout.write('Give me a ' + letters[i] + '!\n'); //more direct way to write to the console;
+  for (var i = 0; i < letters.length; i++) {
+    if (['A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'].indexOf(letters[i]) !== -1) {
+      process.stdout.write('Give me an ' + letters[i] + '!\n');
+    } else {
+      process.stdout.write('Give me a ' + letters[i] + '!\n');
+    };
+  }
 }
