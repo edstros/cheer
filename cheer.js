4f9648b1d71a88d7066db8d7d8df10773dceef3a
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-27 22:57:34 -0500

    final answer

--- cheer.js
+++ cheer.js
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
- //var includes = require('lodash/collection/includes')
+
 var prompt = require('prompt');
 prompt.get('name', function (err, result) {
   printCheer(result.name);
 });
 function printCheer(name) {
-  console.log('All right, it\'s ' + name.toUpperCase() + '!');
-  //separate the letters in the name
+  console.log('All right, it\'s ' + name.toUpperCase() + '!'); //separate the letters in the name
   var letters = name.toUpperCase().split('');
-  //filter removes the white spaces
-  //returned by indexOf
-  letters = letters.filter(function (str) {
+  letters = letters.filter(function (str) { //filter removes the white spaces returned by indexOf
     return str.indexOf(' ');
   });
-  //take each letter in the name and loop
-  //through the test array then print result
-  //the -1 means false and returns the else statement
-  for (var i = 0; i < letters.length; i++) {
+  for (var i = 0; i < letters.length; i++) {//take each letter in the name and loop through the test array then print result
     if (['A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'].indexOf(letters[i]) !== -1) {
       process.stdout.write('Give me an ' + letters[i] + '!\n');
     } else {
