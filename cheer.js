3406ba7cb283566e79d200e27e8459a2ae554cd1
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-28 14:28:55 -0500

    Working tests

--- cheer.js
+++ cheer.js
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
 
+/*
 var prompt = require('prompt');
 prompt.get('name', function (err, result) {
   printCheer(result.name);
 });
-function printCheer(name) {
-  console.log('All right, it\'s ' + name.toUpperCase() + '!'); //separate the letters in the name
-  var letters = name.toUpperCase().split('');
-  letters = letters.filter(function (str) { //filter removes the white spaces returned by indexOf
-    return str.indexOf(' ');
-  });
-  for (var i = 0; i < letters.length; i++) {//take each letter in the name and loop through the test array then print result
-    if (['A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'].indexOf(letters[i]) !== -1) {
-      process.stdout.write('Give me an ' + letters[i] + '!\n');
-    } else {
-      process.stdout.write('Give me a ' + letters[i] + '!\n');
-    };
+*/
+
+/*function printCheer(name) {*/
+//  console.log('All right, it\'s ' + name.toUpperCase() + '!'); //separate the letters in the name
+
+  var args = [];
+
+  for (var i = 2; i < process.argv.length; i++) {
+    args.push(process.argv[i]);
+
+    var letters = args.toString().toUpperCase().split('');
+    letters = letters.filter(function (str) { //filter removes the white spaces returned by indexOf
+      return str.indexOf(' ');
+    });
+    for (var i = 0; i < letters.length; i++) { //take each letter in the name and loop through the test array then print result
+      if (['A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'].indexOf(letters[i]) !== -1) {
+        process.stdout.write('Give me an ' + letters[i] + '!\n');
+      } else {
+        process.stdout.write('Give me a ' + letters[i] + '!\n');
+      };
+    }
   }
-}
+//}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
+
 var should = require('chai').should();
 var cp = require('child_process');
+
+
+
 describe('Tests', function () {
   it('truthyness', function () {
     true.should.equal(true);
@@ -8,13 +13,13 @@ describe('Tests', function () {
 describe.only('CLI', function () {
   it('should handle lowercase letters', function (done) {
     cp.execFile('./cheer.js', ['name'], function (err, stdout) {
-      stdout.should.equal('Give me an N!\n Give me an A!\n Give me an M!\n Give me an E!\n')
+      stdout.should.equal('Give me an N!\nGive me an A!\nGive me an M!\nGive me an E!\n')
       done();
     });
   });
   it('should remove spaces in multiple word names', function (done) {
     cp.execFile('./cheer.js', ['Jim Bob'], function (err, stdout) {
-      stdout.should.equal('Give me a J!\n Give me an I!\n Give me an M!\n Give me a B!\n Give me an O!\n Give me a B!\n')
+      stdout.should.equal('Give me a J!\nGive me an I!\nGive me an M!\nGive me a B!\nGive me an O!\nGive me a B!\n')
       done();
     })
   });
